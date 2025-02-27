3227560e6baec34fa10f92d081fb4e3284240ebd
Jose Arguetaq <jose.argueta@ionnovate.com>
2025-02-26 21:44:35 -0800

    included the rest of the changes to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
 import Cards from "./components/Cards";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-hidden">
+      <Navbar />
       <Hero />
       <Cards />
     </main>
