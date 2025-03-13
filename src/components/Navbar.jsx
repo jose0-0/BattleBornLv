77d1a07d73e482524dace3a6e797b9fc503b9ba6
Jose Arguetaq <jose.argueta@ionnovate.com>
2025-03-12 22:29:46 -0700

    made the nav bar mobile friendly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import { useState, useEffect, useRef } from "react";
 import { useWindowScroll } from "react-use";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import gsap from "gsap";
 
 const navItems = [
   {
+    title: "Home",
+    path: "/",
+  },
+  {
     title: "Services",
-    path: "#services",
+    path: "/#services",
   },
   {
     title: "Reviews",
-    path: "#reviews",
+    path: "/#reviews",
   },
   {
     title: "Gallery",
@@ -23,7 +28,13 @@ const navItems = [
 const Navbar = () => {
   const [lastScrollY, setLastScrollY] = useState(0);
   const [isNavVisible, setIsNavVisible] = useState(true);
+  const [mobileNavVisible, setMobileNavVisible] = useState(false);
   const navContainerRef = useRef(null);
+
+  const toggleNav = () => {
+    setMobileNavVisible(!mobileNavVisible);
+  };
+
   const { y: currentScrollY } = useWindowScroll();
 
   useEffect(() => {
@@ -57,16 +68,47 @@ const Navbar = () => {
         <nav className="flex size-full items-center justify-between p-4">
           <div className="flex items-center gap-7">
             {/* <img src="/images/logo.png" className="w-10"/> */}
-            <span className="text-white">Battle Born Lv</span>
+            <a className="text-white" href="/">
+              Battle Born Lv
+            </a>
           </div>
           <div className="flex h-full items-center">
-            <div className="hidden md:block">
-              {navItems.map((item) => (
-                <a key={item} href={item.path} className="nav-hover-btn">
+            <div className="hidden md:flex">
+              {navItems.map((item, index) => (
+                <a key={index} href={item.path} className="nav-hover-btn">
                   {item.title}
                 </a>
               ))}
             </div>
+          </div>
+          {/* mobile Nav */}
+          <div onClick={toggleNav} className="block md:hidden z-10">
+            {mobileNavVisible ? (
+              <AiOutlineClose size={30} />
+            ) : (
+              <AiOutlineMenu size={30} color="white" />
+            )}
+          </div>
+
+          <div
+            className={
+              mobileNavVisible
+                ? "xl:hidden absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center w-full h-screen bg-white text-black ease-in duration-300 rounded"
+                : "xl:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex flex-col justify-center items-center w-full h-screen text-white ease-in duration-300"
+            }
+          >
+            {navItems.map((link, index) => {
+              return (
+                <a
+                  onClick={toggleNav}
+                  href={link.path}
+                  key={index}
+                  className="p-4 text-4xl duration-300"
+                >
+                  {link.title}
+                </a>
+              );
+            })}
           </div>
         </nav>
       </header>
