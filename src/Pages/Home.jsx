1c5e5103cc036a471e3be78143d46abc809f0152
Jose Arguetaq <jose.argueta@ionnovate.com>
2025-03-19 00:15:58 -0700

    included the rest of the changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import Cards from "../components/Cards";
 import MissionStatement from "../components/MissionStatement";
 import Footer from "../components/Footer";
 import ServiceableAreas from "../components/ServiceableAreas";
+import Testimonials from "../components/Testimonials";
 const Home = () => {
   return (
     <main className="relative min-h-full w-full overflow-hidden">
@@ -12,6 +13,7 @@ const Home = () => {
       <Cards />
       <MissionStatement />
       <ServiceableAreas />
+      <Testimonials />
       <Footer />
     </main>
   );
