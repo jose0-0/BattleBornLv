4baedd02509b61e5082af4a862f95e7e83bc18a9
Jose Arguetaq <jose.argueta@ionnovate.com>
2025-03-19 00:15:15 -0700

    added a review section widget to display battle borns reviews

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,30 @@
+const Testimonials = () => {
+  return (
+    <div id="reviews" className="w-full p-5 pb-16">
+      <div className="flex flex-col justify-center items-center text-center">
+        <h1 className="text-3xl">Discover the Battle Born Difference</h1>
+        <div className="bg-black rounded-md w-[70px] mx-[1rem] h-[2px] my-4" />
+        <span className="text-sm text-thin leading-7 p-5 max-w-[60rem] font-thin">
+          Our satisfied customers speak for themselves—our expert detailing
+          services deliver unmatched quality and protection for your vehicle. At
+          Battle Born LV, we take pride in providing exceptional results,
+          top-tier products, and outstanding customer service. Trust us to keep
+          your vehicle looking its best and experience the Battle Born LV
+          standard today!
+        </span>
+      </div>
+      <div className="p-5">
+        <script
+          src="https://static.elfsight.com/platform/platform.js"
+          async
+        ></script>
+        <div
+          class="elfsight-app-00e9ba95-b450-4321-8ebf-6532b0eec5cb"
+          data-elfsight-app-lazy
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
