1a1dd26661efa15bc28a9e9a357d9041bfc3e28d
Jose Arguetaq <jose.argueta@ionnovate.com>
2025-03-19 22:32:51 -0700

    removed unnecessary features for the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,19 +8,10 @@ const Hero = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 4;
-  const nextVideoRef = useRef(null);
+  const totalVideos = 1;
 
   const handleVideoLoad = () => {
     setLoadedVideos((prev) => prev + 1);
-  };
-
-  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
-
-  const handleMiniVideoClick = () => {
-    setHasClicked(true);
-
-    setCurrentIndex(upcomingVideoIndex);
   };
 
   useEffect(() => {
@@ -28,34 +19,6 @@ const Hero = () => {
       setIsLoading(false);
     }
   }, [loadedVideos]);
-
-  useGSAP(
-    () => {
-      if (hasClicked) {
-        gsap.set("#next-video", { visibility: "visible" });
-
-        gsap.to("#next-video", {
-          transformOrigin: "center center",
-          scale: 1,
-          width: "100%",
-          height: "100%",
-          duration: 1,
-          ease: "power1.inOut",
-          onStart: () => nextVideoRef.current.play(),
-        });
-
-        gsap.from("current-video", {
-          transformOrigin: "center center",
-          scale: 0,
-          duration: 1.5,
-          ease: "power1.inOut",
-        });
-      }
-    },
-    { dependencies: [currentIndex], revertOnUpdate: true }
-  );
-
-  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
   return (
     <div className="relative h-dvh w-full overflow-x-hidden">
@@ -70,44 +33,18 @@ const Hero = () => {
       )}
       <div
         id="video-frame"
-        className="relative z-10 h-dvh w-full overflow-hidden rounded-lg"
+        className="relative z-10 h-dvh w-full overflow-hidden"
       >
         <div>
-          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div
-              onClick={handleMiniVideoClick}
-              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
-            >
-              <video
-                ref={nextVideoRef}
-                src={getVideoSrc(upcomingVideoIndex)}
-                loop
-                muted
-                id="current-video"
-                className="size-64 origin-center scale-150 object-cover object-center"
-                onLoadedData={handleVideoLoad}
-              />
-            </div>
-          </div>
           <video
-            ref={nextVideoRef}
-            src={getVideoSrc(currentIndex)}
-            loop
-            muted
-            id="next-video"
-            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
-            onLoadedData={handleVideoLoad}
-          />
-          <video
-            src={getVideoSrc(
-              currentIndex === totalVideos - 1 ? 1 : currentIndex
-            )}
+            src="/videos/hero-1.mp4"
             autoPlay
             loop
             muted
             className="absolute left-0 top-0 size-full object-cover object-center"
             onLoadedData={handleVideoLoad}
           />
+          <div className="absolute inset-0 bg-black/30" />
         </div>
         <h1 className="hero-heading absolute bottom-5 right-5 z-40 sm:px-10 px-5 text-white">
           Mobile Detailing
@@ -115,7 +52,7 @@ const Hero = () => {
         <div className="absolute left-0 top-0 z-40 size-full ">
           <div className="mt-24 px-5 sm:px-10">
             <h1 className="hero-heading text-white">Battle Born Lv</h1>
-            <p className="mb-5 max-w-64 text-white">
+            <p className="mb-5 max-w-64 text-white font-thin">
               {" "}
               Experience showroom shine, wherever you are.{" "}
             </p>
@@ -135,3 +72,20 @@ const Hero = () => {
 };
 
 export default Hero;
+
+{
+  /* <div
+id="video-frame"
+className="relative z-10 h-dvh w-full overflow-hidden"
+>
+<video
+  src="videos/hero-1.mp4"
+  autoPlay
+  loop
+  muted
+  className="absolute left-0 top-0 size-full object-cover object-center"
+  onLoadedData={handleVideoLoad}
+/>
+<div className="absolute inset-0 bg-black/30" />
+</div> */
+}
